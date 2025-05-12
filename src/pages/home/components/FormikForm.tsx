66a4d0f76a378a973ccc6955b40a7541ba86a2fd
Dien Cap Bao <dien.capbao@sharpensols.com>
2025-05-12 17:53:36 +0700

    feat: Create example form management

--- src/pages/home/components/FormikForm.tsx
+++ src/pages/home/components/FormikForm.tsx
@@ -1,37 +1,226 @@
 import { useFormik } from "formik";
+import { Button } from "primereact/button";
+import { Calendar } from "primereact/calendar";
+import { Checkbox } from "primereact/checkbox";
+import { Dropdown } from "primereact/dropdown";
+import { InputSwitch } from "primereact/inputswitch";
 import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
+import { MultiSelect } from "primereact/multiselect";
+import { RadioButton } from "primereact/radiobutton";
+import * as yup from "yup";
+
+interface AccountFormValues {
+  fullName: string;
+  gender: string;
+  dateOfBirth: Date | null;
+  jobs: string[];
+  category: string;
+  methods: string[];
+  enable: boolean;
+  description: string;
+}
 
 export function FormikForm() {
-  const fieldKeys = Array.from({ length: 50 }, (_, i) => `full_name_${i + 1}`);
-
-  const initialValues = fieldKeys.reduce((acc, key) => {
-    acc[key] = "";
-    return acc;
-  }, {} as Record<string, string>);
-
-  const formik = useFormik({
-    initialValues,
-    onSubmit: (values: any) => console.log(values),
+  const formik = useFormik<AccountFormValues>({
+    initialValues: {
+      fullName: "",
+      gender: "",
+      dateOfBirth: null,
+      jobs: [],
+      category: "",
+      methods: [],
+      enable: false,
+      description: "",
+    },
+    validationSchema: yup.object({
+      fullName: yup.string().required(),
+      gender: yup.string().required(),
+      dateOfBirth: yup.date().required(),
+      jobs: yup.array().min(1, "At least one job is required"),
+      category: yup.string().required(),
+      methods: yup.array().min(1, "At least one method is required"),
+      enable: yup.boolean().required(),
+      description: yup.string().required(),
+    }),
+    validateOnChange: false,
+    onSubmit: (values) => console.log(values),
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form onSubmit={formik.handleSubmit} className="p-4">
       <h1 className="text-2xl text-center mb-4">Formik - Form</h1>
+
+      {/* <div>{JSON.stringify(formik.errors)}</div> */}
       <div className="grid grid-cols-4 gap-4">
-        {fieldKeys.map((key) => (
+        <div className="space-y-1">
+          <label className="block mb-1">Fullname</label>
           <InputText
-            key={key}
-            name={key}
-            placeholder={key}
+            name="fullName"
+            placeholder="fullName"
             className="w-full"
-            value={formik.values[key]}
+            invalid={!!formik.errors.fullName}
+            value={formik.values.fullName}
             onChange={formik.handleChange}
           />
-        ))}
+          {!!formik.errors.fullName && (
+            <small className="p-error">{formik.errors.fullName}</small>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label className="block mb-1">Gender</label>
+          <Dropdown
+            name="gender"
+            className="w-full"
+            options={[
+              { label: "Male", value: "male" },
+              { label: "Female", value: "female" },
+              { label: "Other", value: "other" },
+            ]}
+            optionLabel="label"
+            placeholder="gender"
+            showClear={true}
+            invalid={!!formik.errors.gender}
+            value={formik.values.gender}
+            onChange={formik.handleChange}
+          />
+          {!!formik.errors.gender && (
+            <small className="p-error">{formik.errors.gender}</small>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label className="block mb-1">DateOfBirth</label>
+          <Calendar
+            name="dateOfBirth"
+            className="w-full"
+            placeholder="DateOfBirth"
+            showIcon
+            panelClassName="!z-[9999]"
+            invalid={!!formik.errors.dateOfBirth}
+            value={formik.values.dateOfBirth}
+            onChange={formik.handleChange}
+          />
+          {!!formik.errors.dateOfBirth && (
+            <small className="p-error">{formik.errors.dateOfBirth}</small>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label className="block mb-1">Jobs</label>
+          <MultiSelect
+            name="jobs"
+            options={[
+              { label: "Developer", value: "developer" },
+              { label: "Designer", value: "designer" },
+              { label: "Manager", value: "manager" },
+              { label: "Tester", value: "tester" },
+              { label: "DevOps", value: "devops" },
+            ]}
+            optionLabel="label"
+            display="chip"
+            placeholder="Jobs"
+            showClear={formik.values.jobs?.length > 0}
+            maxSelectedLabels={3}
+            filter
+            className="w-full"
+            invalid={!!formik.errors.jobs}
+            value={formik.values.jobs}
+            onChange={formik.handleChange}
+          />
+          {!!formik.errors.jobs && (
+            <small className="p-error">{formik.errors.jobs}</small>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label className="block mb-1">Category</label>
+          {[
+            { name: "Accounting", value: "A" },
+            { name: "Marketing", value: "M" },
+            { name: "Production", value: "P" },
+          ].map((option) => (
+            <div key={option.value} className="flex items-center">
+              <RadioButton
+                inputId={option.value}
+                name="category"
+                value={option.value}
+                checked={formik.values.category === option.value}
+                invalid={!!formik.errors.category}
+                onChange={formik.handleChange}
+              />
+              <label htmlFor={option.value} className="block ml-2">
+                {option.name}
+              </label>
+            </div>
+          ))}
+          {!!formik.errors.category && (
+            <small className="p-error">{formik.errors.category}</small>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label className="block mb-1">Methods</label>
+          {[
+            { name: "GET", value: "get" },
+            { name: "POST", value: "post" },
+            { name: "PUT", value: "put" },
+            { name: "DELETE", value: "delete" },
+          ].map((option) => (
+            <div key={option.value} className="flex items-center">
+              <Checkbox
+                inputId={option.value}
+                name="methods"
+                value={option.value}
+                checked={formik.values.methods.includes(option.value)}
+                invalid={!!formik.errors.methods}
+                onChange={formik.handleChange}
+              />
+              <label htmlFor={option.value} className="block ml-2">
+                {option.name}
+              </label>
+            </div>
+          ))}
+          {!!formik.errors.methods && (
+            <small className="p-error">{formik.errors.methods}</small>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label className="block mb-1">Accept Terms</label>
+          <InputSwitch
+            name="enable"
+            checked={formik.values.enable}
+            onChange={formik.handleChange}
+            invalid={!!formik.errors.enable}
+          />
+          {!!formik.errors.enable && (
+            <small className="p-error">{formik.errors.enable}</small>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <label className="block mb-1">Description</label>
+          <InputTextarea
+            name="description"
+            placeholder="Description"
+            rows={5}
+            cols={30}
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            className="w-full"
+            invalid={!!formik.errors.description}
+          />
+          {!!formik.errors.description && (
+            <small className="p-error">{formik.errors.description}</small>
+          )}
+        </div>
       </div>
-      <button type="submit" className="mt-6 p-2 bg-blue-500 text-white rounded">
+
+      <Button type="submit" className="mt-4">
         Submit
-      </button>
+      </Button>
     </form>
   );
 }
--- src/pages/home/components/RHFForm.tsx
+++ src/pages/home/components/RHFForm.tsx
@@ -1,52 +1,245 @@
-import { Form, FormControl, FormField, FormItem } from "@/components/form";
+import { Calendar } from "primereact/calendar";
+import { Checkbox } from "primereact/checkbox";
+import { Dropdown } from "primereact/dropdown";
+import { InputSwitch } from "primereact/inputswitch";
 import { InputText } from "primereact/inputtext";
-import { useForm } from "react-hook-form";
+import { InputTextarea } from "primereact/inputtextarea";
+import { MultiSelect } from "primereact/multiselect";
+import { RadioButton } from "primereact/radiobutton";
+import { Controller, useForm } from "react-hook-form";
+
+interface AccountFormValues {
+  fullName: string;
+  gender: string;
+  dateOfBirth: Date | null;
+  jobs: string[];
+  category: string;
+  methods: string[];
+  enable: boolean;
+  description: string;
+}
 
 export function RHFForm() {
-  console.log("RHFForm render");
-  const fieldKeys = Array.from({ length: 50 }, (_, i) => `full_name_${i + 1}`);
-
-  const initialValues = fieldKeys.reduce((acc, key) => {
-    acc[key] = "";
-    return acc;
-  }, {} as Record<string, string>);
-
-  const form = useForm({
-    defaultValues: initialValues,
+  const form = useForm<AccountFormValues>({
+    defaultValues: {
+      fullName: "",
+      gender: "",
+      dateOfBirth: null,
+      jobs: [],
+      category: "",
+      methods: [],
+      enable: false,
+      description: "",
+    },
   });
 
   return (
-    <Form {...form}>
+    <form onSubmit={form.handleSubmit((values) => console.log(values))}>
       <h1 className="text-2xl text-center mb-4">RHF - Form</h1>
-      <form onSubmit={form.handleSubmit((values) => console.log(values))}>
-        <div className="grid grid-cols-4 gap-4">
-          {fieldKeys.map((key) => (
-            <FormField
-              key={key}
-              control={form.control}
-              name={key}
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <InputText
-                      key={key}
-                      placeholder={key}
-                      className="w-full"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
+      <div className="grid grid-cols-4 gap-4">
+        <Controller
+          control={form.control}
+          name="fullName"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">Fullname</label>
+              <InputText
+                placeholder="fullName"
+                className="w-full"
+                invalid={fieldState.invalid}
+                {...field}
+              />
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
               )}
-            />
-          ))}
-        </div>
-        <button
-          type="submit"
-          className="mt-6 p-2 bg-blue-500 text-white rounded"
-        >
-          Submit
-        </button>
-      </form>
-    </Form>
+            </div>
+          )}
+        />
+
+        <Controller
+          control={form.control}
+          name="gender"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">Gender</label>
+              <Dropdown
+                className="w-full"
+                options={[
+                  { label: "Male", value: "male" },
+                  { label: "Female", value: "female" },
+                  { label: "Other", value: "other" },
+                ]}
+                optionLabel="label"
+                placeholder="gender"
+                showClear={true}
+                invalid={fieldState.invalid}
+                {...field}
+              />
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
+              )}
+            </div>
+          )}
+        />
+
+        <Controller
+          control={form.control}
+          name="dateOfBirth"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">DateOfBirth</label>
+              <Calendar
+                className="w-full"
+                placeholder="DateOfBirth"
+                showIcon
+                panelClassName="!z-[9999]"
+                invalid={fieldState.invalid}
+                {...field}
+              />
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
+              )}
+            </div>
+          )}
+        />
+
+        <Controller
+          control={form.control}
+          name="jobs"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">Jobs</label>
+              <MultiSelect
+                options={[
+                  { label: "Developer", value: "developer" },
+                  { label: "Designer", value: "designer" },
+                  { label: "Manager", value: "manager" },
+                  { label: "Tester", value: "tester" },
+                  { label: "DevOps", value: "devops" },
+                ]}
+                optionLabel="label"
+                display="chip"
+                placeholder="Jobs"
+                showClear={field.value?.length > 0}
+                maxSelectedLabels={3}
+                filter
+                className="w-full"
+                invalid={fieldState.invalid}
+                {...field}
+              />
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
+              )}
+            </div>
+          )}
+        />
+
+        <Controller
+          control={form.control}
+          name="category"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">Category</label>
+              {[
+                { name: "Accounting", value: "A" },
+                { name: "Marketing", value: "M" },
+                { name: "Production", value: "P" },
+              ].map((option) => (
+                <div key={option.value} className="flex items-center">
+                  <RadioButton
+                    inputId={option.value}
+                    checked={field.value === option.value}
+                    invalid={fieldState.invalid}
+                    {...field}
+                  />
+                  <label htmlFor={option.value} className="block ml-2">
+                    {option.name}
+                  </label>
+                </div>
+              ))}
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
+              )}
+            </div>
+          )}
+        />
+
+        <Controller
+          control={form.control}
+          name="methods"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">Methods</label>
+              {[
+                { name: "GET", value: "get" },
+                { name: "POST", value: "post" },
+                { name: "PUT", value: "put" },
+                { name: "DELETE", value: "delete" },
+              ].map((option) => (
+                <div key={option.value} className="flex items-center">
+                  <Checkbox
+                    inputId={option.value}
+                    checked={field.value.includes(option.value)}
+                    invalid={fieldState.invalid}
+                    {...field}
+                  />
+                  <label htmlFor={option.value} className="block ml-2">
+                    {option.name}
+                  </label>
+                </div>
+              ))}
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
+              )}
+            </div>
+          )}
+        />
+
+        <Controller
+          control={form.control}
+          name="enable"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">Accept Terms</label>
+              <InputSwitch
+                checked={field.value}
+                invalid={fieldState.invalid}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+              />
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
+              )}
+            </div>
+          )}
+        />
+
+        <Controller
+          control={form.control}
+          name="description"
+          render={({ field, fieldState }) => (
+            <div className="space-y-1">
+              <label className="block mb-1">Description</label>
+              <InputTextarea
+                name="description"
+                placeholder="Description"
+                rows={5}
+                cols={30}
+                className="w-full"
+                invalid={fieldState.invalid}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+              />
+              {!!fieldState.error?.message && (
+                <small className="p-error">{fieldState.error?.message}</small>
+              )}
+            </div>
+          )}
+        />
+      </div>
+      <button type="submit" className="mt-6 p-2 bg-blue-500 text-white rounded">
+        Submit
+      </button>
+    </form>
   );
 }
